756ec0c82faabdd39bdc066d96a49e6bf1ba784a
ChloeBer <47952181+ChloeBer@users.noreply.github.com>
2019-03-14 15:24:56 +0100

    Update test_graph.py

--- test_graph.py
+++ test_graph.py
@@ -2,6 +2,7 @@ __author__ = "Bernardet, Coruzzi, Laik, Montfort, Rouby, Trouyet"
 __filename__ = "test_graph"
 
 from graph import *
+from class_point import *
 import unittest
 from copy import *
 
@@ -11,24 +12,30 @@ from math import inf
 
 class GraphTest(unittest.TestCase):
     def setUp(self):
+        # Points
+        self.point1 = Point(0, 1)
+        self.point2 = Point(0, 2)
+        self.point3 = Point(0, 3)
+        self.point4 = Point(0, 4)
+
         # Vertices
-        self.vertices1 = [1]
-        self.vertices2 = [1, 2]
-        self.vertices3 = [1, 2, 3]
-        self.vertices4 = [1, 2, 3, 4]
+        self.vertices1 = [self.point1]
+        self.vertices2 = [self.point1, self.point2]
+        self.vertices3 = [self.point1, self.point2, self.point3]
+        self.vertices4 = [self.point1, self.point2, self.point3, self.point4]
 
         # Edges
-        self.edges12_1 = [(1, 2, 3.)]
-        self.edges12_2 = [(2, 1, 5.)]
-        self.edges123_1 = [(1, 2, 3.), (1, 3, 5.)]
-        self.edges123_2 = [(1, 2, 3.), (1, 3, 5.), (2, 3, 7.)]
-        self.edges123_3 = [(1, 2, 3.), (2, 3, 7.)]
-        self.edges123_4 = [(2, 3, 7.)]
-        self.edges1234_1 = [(1, 2, 3.), (1, 3, 5.), (1, 4, 7.)]
-        self.edges1234_2 = [(1, 2, 3.), (1, 3, 5.), (2, 3, 10.), (1, 4, 7.)]
-        self.edges1234_3 = [(1, 2, 3.), (1, 3, 5.), (2, 3, 10.), (1, 4, 7.), (2, 4, 7.)]
-        self.edges1234_4 = [(1, 2, 3.), (1, 3, 5.), (2, 3, 10.), (1, 4, 7.), (2, 4, 7.), (4, 3, 5.)]
-        self.edges1234_5 = [(1, 3, 5.), (2, 3, 10.), (1, 4, 7.), (2, 4, 7.), (4, 3, 5.)]
+        self.edges12_1 = [(self.point1, self.point2, 3.)]
+        self.edges12_2 = [(self.point2, self.point1, 5.)]
+        self.edges123_1 = [(self.point1, self.point2, 3.), (self.point1, self.point3, 5.)]
+        self.edges123_2 = [(self.point1, self.point2, 3.), (self.point1, self.point3, 5.), (self.point2, self.point3, 7.)]
+        self.edges123_3 = [(self.point1, self.point2, 3.), (self.point2, self.point3, 7.)]
+        self.edges123_4 = [(self.point2, self.point3, 7.)]
+        self.edges1234_1 = [(self.point1, self.point2, 3.), (self.point1, self.point3, 5.), (self.point1, self.point4, 7.)]
+        self.edges1234_2 = [(self.point1, self.point2, 3.), (self.point1, self.point3, 5.), (self.point2, self.point3, 10.), (self.point1, self.point4, 7.)]
+        self.edges1234_3 = [(self.point1, self.point2, 3.), (self.point1, self.point3, 5.), (self.point2, self.point3, 10.), (self.point1, self.point4, 7.), (self.point2, self.point4, 7.)]
+        self.edges1234_4 = [(self.point1, self.point2, 3.), (self.point1, self.point3, 5.), (self.point2, self.point3, 10.), (self.point1, self.point4, 7.), (self.point2, self.point4, 7.), (self.point4, self.point3, 5.)]
+        self.edges1234_5 = [(self.point1, self.point3, 5.), (self.point2, self.point3, 10.), (self.point1, self.point4, 7.), (self.point2, self.point4, 7.), (self.point4, self.point3, 5.)]
 
         # Graphs
         self.emptygraph = Graph([], [])
@@ -79,14 +86,14 @@ class GraphTest(unittest.TestCase):
     def testSetVerticesErrorList(self):
         graph = copy(self.emptygraph)
         try:
-            value = graph.setVertices(1)
+            value = graph.setVertices(copy(self.point1))
         except:
             self.assertRaises(AssertionError)
 
-    def testSetVerticesErrorVertexNotInt(self):
+    def testSetVerticesErrorVerticesNotPoint(self):
         graph = copy(self.emptygraph)
         try:
-            value = graph.setVertices(["a"])
+            value = graph.setVertices(["a", 1])
         except:
             self.assertRaises(AssertionError)
 
@@ -100,142 +107,142 @@ class GraphTest(unittest.TestCase):
     def testSetEdgesErrorList(self):
         graph = copy(self.graph12_1)
         try:
-            value = graph.setEdges(((1, 2, 3.)))
+            value = graph.setEdges(((Point(1, 0), Point(2, 0), 3.)))
         except:
             self.assertRaises(AssertionError)
 
     def testSetEdgesErrorOneEdgeNotTuple(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.setEdges([(1, 2, 3.), [1, 3, 3.]])
+            value = graph.setEdges([(copy(self.point1), copy(self.point2), 3.), [copy(self.point1), copy(self.point3), 3.]])
         except:
             self.assertRaises(AssertionError)
 
     def testSetEdgesErrorOneLenEdgeNot3(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.setEdges([(1, 2, 3.), (1, 3)])
+            value = graph.setEdges([(copy(self.point1), copy(self.point2), 3.), (copy(self.point1), copy(self.point3))])
         except:
             self.assertRaises(AssertionError)
 
     def testSetEdgesErrorVertexNotInVertices(self):
         graph = copy(self.graph12_1)
         try:
-            value = graph.setEdges([(1, 2, 3.), (1, 3, 3.)])
+            value = graph.setEdges([(copy(self.point1), copy(self.point2), 3.), (copy(self.point1), copy(self.point3), 3.)])
         except:
             self.assertRaises(AssertionError)
 
     def testSetEdgesErrorPoidsNotFloat(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.setEdges([(1, 2, 3.), (1, 3, "a")])
+            value = graph.setEdges([(copy(self.point1), copy(self.point2), 3.), (copy(self.point1), copy(self.point3), "a")])
         except:
             self.assertRaises(AssertionError)
 
     def testSetEdgesErrorOneEdgeBetweenSameVertex(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.setEdges([(1, 2, 3.), (1, 1, 5.)])
+            value = graph.setEdges([(copy(self.point1), copy(self.point2), 3.), (copy(self.point1), copy(self.point1), 5.)])
         except:
             self.assertRaises(AssertionError)
 
     def testSetEdgesErrorTwoEdgesBetweenSameVertices(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.setEdges([(1, 2, 3.), (2, 1, 5.)])
+            value = graph.setEdges([(copy(self.point1), copy(self.point2), 3.), (copy(self.point2), copy(self.point1), 5.)])
         except:
             self.assertRaises(AssertionError)
 
     def testAddVertexEmptyGraph(self):
         graph = copy(self.emptygraph)
-        graph.add_vertex(1)
+        graph.add_vertex(copy(self.point1))
         value = graph.getVertices()
-        expected = copy(self.vertices1)  # [1]
+        expected = copy(self.vertices1)  # point1
         self.assertEqual(value, expected)
 
-    def testAddVertexErrorVertexNotInt(self):
+    def testAddVertexErrorVertexNotPoint(self):
         graph = copy(self.emptygraph)
         try:
-            value = graph.add_vertex([1, "a"])
+            value = graph.add_vertex([copy(self.point1), "a"])
         except:
             self.assertRaises(AssertionError)
 
     def testAddVertexAlreadyInVertices(self):
         graph = copy(self.graph1)
-        graph.add_vertex(1)
+        graph.add_vertex(copy(self.point1))
         value = graph.getVertices()  # inchange
         expected = copy(self.vertices1)
         self.assertEqual(value, expected)
 
     def testAddEdgeWithNodesAlreadyInVertices(self):
         graph = copy(self.graph123_1)
-        graph.add_edge(2, 3, 7.)
+        graph.add_edge(copy(self.point2), copy(self.point3), 7.)
         value = graph.getEdges()
-        expected = copy(self.edges123_1) + [(2, 3, 7.)]
+        expected = copy(self.edges123_1) + [(copy(self.point2), copy(self.point3), 7.)]
         self.assertEqual(value, expected)
 
     def testAddEdgeWithNodesNotInVertices(self):
         graph = copy(self.emptygraph)
-        graph.add_edge(2, 3, 7.)
+        graph.add_edge(copy(self.point2), copy(self.point3), 7.)
         value_edges = graph.getEdges()
-        expected_edges = [(2, 3, 7.)]
+        expected_edges = [(copy(self.point2), copy(self.point3), 7.)]
         self.assertEqual(value_edges, expected_edges)
         value_vertices = graph.getVertices()
-        expected_vertices = [2, 3]
+        expected_vertices = [copy(self.point2), copy(self.point3)]
         self.assertEqual(value_vertices, expected_vertices)
 
     def testAddEdgeErrorAlreadyEdgeBetweenSameVertices(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.add_edge(2, 1, 10.)  # arete (1, 2, 3.) dans graph
+            value = graph.add_edge(copy(self.point2), copy(self.point1), 10.)  # arete (copy(self.point1), copy(self.point2), 3.) dans graph
         except:
             self.assertRaises(AssertionError)
 
     def testAddEdgeErrorEdgeAlreadyInEdges(self):
         graph = copy(self.graph123_1)
         try:
-            graph.add_edge(1, 2, 3.)
+            graph.add_edge(copy(self.point1), copy(self.point2), 3.)
         except:
             self.assertRaises(AssertionError)
 
     def testAddEdgeErrorEdgeAlreadyInEdgesWithVertex1And2Reverse(self):
         graph = copy(self.graph123_1)
         try :
-            value = graph.add_edge(2, 1, 3.)  # arete (1, 2, 3.) dans Edges
+            value = graph.add_edge(copy(self.point2), copy(self.point1), 3.)  # arete (copy(self.point1), copy(self.point2), 3.) dans Edges
         except:
             self.assertRaises(AssertionError)
 
     def testAddEdgeErrorEdgeBetweenSameVertex(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.add_edge(2, 2, 3.)
+            value = graph.add_edge(copy(self.point2), copy(self.point2), 3.)
         except:
             self.assertRaises(AssertionError)
 
-    def testAddEdgeErrorSommet1NotInt(self):
+    def testAddEdgeErrorSommet1NotPoint(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.add_edge([1], 2, 3.)
+            value = graph.add_edge([1], copy(self.point2), 3.)
         except:
             self.assertRaises(AssertionError)
 
-    def testAddEdgeErrorSommet2NotInt(self):
+    def testAddEdgeErrorSommet2NotPoint(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.add_edge(1, "a", 3.)
+            value = graph.add_edge(copy(self.point1), "a", 3.)
         except:
             self.assertRaises(AssertionError)
 
-    def testAddEdgeErrorPoidsNotInt(self):
+    def testAddEdgeErrorPoidsNotFloat(self):
         graph = copy(self.graph123_1)
         try:
-            value = graph.add_edge(1, 2, "b")
+            value = graph.add_edge(copy(self.point1), copy(self.point2), "b")
         except:
             self.assertRaises(AssertionError)
 
     def testSuppEdgeGraph12_1(self):
         graph = copy(self.graph12_1)
-        graph.supp_edge((1, 2, 3.))
+        graph.supp_edge((copy(self.point1), copy(self.point2), 3.))
         value = graph.getEdges()
         expected = []
         self.assertEqual(value, expected)
@@ -243,21 +250,21 @@ class GraphTest(unittest.TestCase):
     def testSuppEdgeErrorEdgeNotInEdges(self):
         graph = copy(self.graph12_1)
         try:
-            value = graph.supp_edge((1, 2, 30.))
+            value = graph.supp_edge((copy(self.point1), copy(self.point2), 30.))
         except:
             self.assertRaises(AssertionError)
 
     def testSuppEdgeErrorEdgeNotTuple(self):
         graph = copy(self.graph12_1)
         try:
-            value = graph.supp_edge([1, 2, 3.])
+            value = graph.supp_edge([copy(self.point1), copy(self.point2), 3.])
         except:
             self.assertRaises(AssertionError)
 
     def testSuppVertexGraph1(self):
         """ graphe sans arete donc edges inchangee"""
         graph = copy(self.graph1)
-        graph.supp_vertex(1)
+        graph.supp_vertex(copy(self.point1))
         value = graph.getVertices()
         expected = []
         self.assertEqual(value, expected)
@@ -265,9 +272,9 @@ class GraphTest(unittest.TestCase):
     def testSuppVertexGraph12_1(self):
         """ graphe avec arete donc supprimer un sommet, supprime les aretes associees au sommet """
         graph = copy(self.graph12_1)
-        graph.supp_vertex(1)
+        graph.supp_vertex(copy(self.point1))
         value_vertices = graph.getVertices()
-        expected_vertices = [2]
+        expected_vertices = [copy(self.point2)]
         self.assertEqual(value_vertices, expected_vertices)
         value_edges = graph.getEdges()
         expected_edges = []
@@ -276,7 +283,7 @@ class GraphTest(unittest.TestCase):
     def testSuppVertexErrorVertexNotInVertices(self):
         graph = copy(self.graph1)
         try:
-            value = graph.supp_vertex(5)
+            value = graph.supp_vertex(copy(self.point4))
         except:
             self.assertRaises(AssertionError)
 
@@ -297,11 +304,42 @@ class GraphTest(unittest.TestCase):
     def testSuccesseursGraph123_1(self):
         graph = copy(self.graph123_1)
         value = graph.successeurs()
-        expected = [[(2, 3.), (3, 5.)], [(1, 3.)], [(1, 5.)]]
+        expected = [[(copy(self.point2), 3.), (copy(self.point3), 5.)], [(copy(self.point1), 3.)], [(copy(self.point1), 5.)]]
         self.assertEqual(value, expected)
 
     def testSuccesseursGraph1234_2(self):
         graph = copy(self.graph1234_2)
         value = graph.successeurs()
-        expected = [[(2, 3.), (3, 5.), (4, 7.)], [(1, 3.), (3, 10.)], [(1, 5.), (2, 10.)], [(1, 7.)]]
+        expected = [[(copy(self.point2), 3.), (copy(self.point3), 5.), (copy(self.point4), 7.)], [(copy(self.point1), 3.), (copy(self.point3), 10.)], [(copy(self.point1), 5.), (copy(self.point2), 10.)], [(copy(self.point1), 7.)]]
         self.assertEqual(value, expected)
+
+    def testEstAtteint(self):
+        graph = copy(self.graph1234_1)
+        value = graph.est_atteint(copy(self.point3), copy(self.point4))
+        self.assertTrue(value)
+
+    def testEstPasAtteint(self):
+        graph = copy(self.graph123_4)
+        value = graph.est_atteint(copy(self.point1), copy(self.point3))
+        self.assertFalse(value)
+
+    def testEstAtteintErrorVertexNotInGraph2(self):
+        graph = copy(self.graph12_1)
+        try:
+            graph.est_atteint(copy(self.point1), copy(self.point3))
+        except:
+            self.assertRaises(AssertionError)
+
+    def testEstAtteintErrorVertexNotInGraph1(self):
+        graph = copy(self.graph12_1)
+        try:
+            graph.est_atteint(copy(self.point3), copy(self.point1))
+        except:
+            self.assertRaises(AssertionError)
+
+    def testEstAtteintErrorGraphEmpty(self):
+        graph = copy(self.emptygraph)
+        try:
+            graph.est_atteint(copy(self.point1), copy(self.point2))
+        except:
+            self.assertRaises(AssertionError)
